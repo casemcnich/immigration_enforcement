cd3e176c2cfe7a17cd283f54d7f363573cbbf6aa
casemcnich <96758009+casemcnich@users.noreply.github.com>
2025-12-03 11:27:23 -0600

    feat: break ipums data into race ethnicity and foreign born status comparison groups

--- code/ipums_employment.R
+++ code/ipums_employment.R
@@ -23,7 +23,6 @@ library("blscrapeR")
 # wd ----
 setwd("C:/Users/casem/Desktop/immigration/immigration_enforcement")
 
-
 # NOTE: To load data, you must download both the extract's data and the DDI
 # and also set the working directory to the folder with these files (or change the path below).
 
@@ -32,6 +31,44 @@ if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr pack
 ddi <- read_ipums_ddi("../data/cps_00027.xml")
 data <- read_ipums_micro(ddi)
 
+mean(data$HOURWAGE_CPIU_2010)
 # clean ipums data -----
+# drop people who are not in the labor force at all
+data <- subset(data, HOURWAGE_CPIU_2010 == 99999.99)
+
+# Foreign-born non-citizen Hispanics  --------------------------------
+
+#foreign born 
+foreign_hisp <- subset(data, NATIVITY == 5 )
+
+# hispanic 
+foreign_hisp <- subset(foreign_hisp, HISPAN >= 100 & HISPAN <= 900)
+
+# non citizen 
+foreign_hisp_noncitizen <- subset(foreign_hisp, CITIZEN == 5)
+
+# Foreign-born naturalized citizen Hispanics  --------------------------------
+foreign_hisp_naturalized <- subset(foreign_hisp, CITIZEN == 4)
+
+# Native-born Hispanics --------------------------------
+# hispanic 
+native_hisp <- subset(data, HISPAN >= 100 & HISPAN <= 900)
+
+# native 
+foreign_hisp <- subset(native_hisp, NATIVITY == 1 |NATIVITY == 2|NATIVITY == 3|NATIVITY == 4)
+
+# Native-born non-Hispanic whites --------------------------------
+
+# native born 
+native_nonhisp_white <-  subset(data, NATIVITY == 1 |NATIVITY == 2|NATIVITY == 3|NATIVITY == 4)
+
+# non hispanic white
+native_nonhisp_white <- subset (native_nonhisp_white, HISPAN == 000)
+
+# white
+native_nonhisp_white <- subset (native_nonhisp_white, RACE == 100)
+
+
+
 # merge ipums data------
 
