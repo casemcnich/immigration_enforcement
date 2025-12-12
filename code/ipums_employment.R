1328424597cca18719749597b5eb9680c8d6c5ca
casemcnich <96758009+casemcnich@users.noreply.github.com>
2025-12-11 19:56:16 -0600

    feat: clean ipums wages

--- code/ipums_employment.R
+++ code/ipums_employment.R
@@ -36,6 +36,14 @@ mean(data$HOURWAGE_CPIU_2010)
 # drop people who are not in the labor force at all
 data <- subset(data, HOURWAGE_CPIU_2010 == 99999.99)
 
+data <- subset(data, COUNTY!= 0)
+
+# merge ipums data----------------------------
+load("../data/foia_df.Rdata")
+
+# merge 
+data <- merge(foia_df, data, by.x = "FIPS", by.y = "COUNTY", all.y = T)
+
 #* Foreign-born non-citizen Hispanics  --------------------------------
 
 #foreign born 
@@ -68,12 +76,53 @@ native_nonhisp_white <- subset (native_nonhisp_white, HISPAN == 000)
 # white
 native_nonhisp_white <- subset (native_nonhisp_white, RACE == 100)
 
+native_nonhisp_white <- subset(native_nonhisp_white,  NATIVITY == 1 |NATIVITY == 2|NATIVITY == 3|NATIVITY == 4)
 
-# merge ipums data------
+# event studies ----------------
+# Event study -----
+#* cleaning 
+
+# Calculate the difference in months between 'yearmon' and 'I247_date' as an integer
+full_df_employ <- full_df_employ %>%
+  mutate(
+    # Create a date from the YEAR and month columns (assume day = 1)
+    current_date = make_date(year, month, 1),
+    
+    # Calculate whole months difference (handles NAs safely)
+    months_since_I247 = if_else(
+      !is.na(month_247a_county),
+      interval(month_247a_county, current_date) %/% months(1),
+      NA_integer_
+    )
+  )
+
+#creating a treatment variable for ever treated and always treated
+full_df_employ <- full_df_employ %>% mutate(treat = ifelse(!is.na(c(month_247a_county)), 1, 0)) 
+
+#* arrests event study -----
+
+event_subset <- subset(full_df_employ, month_247a_county > "2014-01-01")
+
+event_subset <- subset(event_subset, months_since_I247 < 20)
+event_subset <- subset(event_subset, months_since_I247 > -20)
+
+event_subset$z_score_cound <- (event_subset$count - mean(event_subset$count)) / sd(event_subset$count)
+event_subset$z_score_cons <- (event_subset$monthly_emplvl_const - mean(event_subset$monthly_emplvl_const)) / sd(event_subset$monthly_emplvl_const)
+
+event_subset$current_date_factor <- as.factor(event_subset$current_date)
+
+event <- feols(
+  count~ i(months_since_I247, treat, ref = -1)+ factor(current_date_factor) + state | County.x,
+  data = event_subset,   cluster = ~ County.x)
+
+summary(event)
+
+
+#* cleaning --------------- 
 
 
 
 
-# load trac data 
+
 
 
